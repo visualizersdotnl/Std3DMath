3d2151fdc7b4fa4a9d9764b414508b95bf02fce5
njdewit <ndewit@gmail.com>
2019-09-24 16:28:55 +0200

    Update Dependencies.h

--- Dependencies.h
+++ Dependencies.h
@@ -4,12 +4,12 @@
 #define S3D_INLINE __inline
 
 // CRT & STL:
-#include <cfloat>
-#include <cmath>
+#include <cfloat>    // epsilon
+#include <cmath>     // std::truncf()
 #include <assert.h>
 #include <string.h>  // memcpy()
 #include <math.h>    // sinf(), cosf(), et cetera
-#include <algorithm> // std::min, std::max
+#include <algorithm> // std::min(), std::max()
 
 // Firstly to align Vector3/Vector4, and enable SIMD on some of these primitives.
 #include <xmmintrin.h>
