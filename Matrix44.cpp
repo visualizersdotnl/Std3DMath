01171d2f412ee705eb3c9e25f91bdc6d5b9b2ba0
Niels J. de Wit <ndewit@gmail.com>
2026-02-13 14:56:12 +0100

    Fixed determinant zero check in general matrix inverse

--- Matrix44.cpp
+++ Matrix44.cpp
@@ -316,16 +316,16 @@ const Matrix44 Matrix44::GeneralInverse() const
 	pInv[11] = -pSrc[0] * pSrc[ 5] * pSrc[11] + pSrc[0] * pSrc[ 7] * pSrc[ 9] + pSrc[4] * pSrc[1] * pSrc[11] - pSrc[4] * pSrc[3] * pSrc[ 9] - pSrc[ 8] * pSrc[1] * pSrc[ 7] + pSrc[ 8] * pSrc[3] * pSrc[ 5];
 	pInv[15] =  pSrc[0] * pSrc[ 5] * pSrc[10] - pSrc[0] * pSrc[ 6] * pSrc[ 9] - pSrc[4] * pSrc[1] * pSrc[10] + pSrc[4] * pSrc[2] * pSrc[ 9] + pSrc[ 8] * pSrc[1] * pSrc[ 6] - pSrc[ 8] * pSrc[2] * pSrc[ 5];
 	 
-	float determinant = pSrc[0]*pInv[0] + pSrc[1]*pInv[4] + pSrc[2]*pInv[8] + pSrc[3]*pInv[12];
- 	if (0.f == determinant)
+	const float determinant = pSrc[0]*pInv[0] + pSrc[1]*pInv[4] + pSrc[2]*pInv[8] + pSrc[3]*pInv[12];
+ 	if (fabsf(determinant < kEpsilon))
  	{
- 		// FIXME: assert?
+		// Impossible, this matrix eliminates a dimension (FIXME: std::optional<T>?)
  		return Matrix44::Identity();
  	}
 
-	determinant = 1.f/determinant;
+	const float invDet = 1.f/determinant;
 	for (unsigned int iElem = 0; iElem < 16; ++iElem)
-		pInv[iElem] *= determinant;
+		pInv[iElem] *= invDet;
 
 	return matrix;
 }
