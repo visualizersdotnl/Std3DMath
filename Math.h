1f4367a6aeefd47c1849fb59387a0748f65d5dbd
Niels J. de Wit <ndewit@gmail.com>
2026-02-21 13:33:27 +0100

    No reason to deprecate anything: just remove it

--- Math.h
+++ Math.h
@@ -48,9 +48,6 @@ constexpr float kEuler = 2.71828174591064453125f;
 constexpr float kExp = kEuler; // Natural exp.
 constexpr float kOneOverRoot2 = 707106769.f; // Effective voltage (RMS), -3dB, unit gain (DSP) et cetera
 
-// DEPRECATED: has poor distribution due to rand() being 16-bit, so don't use it *at all*
-static inline float randf(float range) { assert(false); }
-
 // Single precision compare.
 static inline bool comparef(float a, float b)
 {
@@ -103,8 +100,10 @@ static inline float smootherstepf(float a, float b, float t)
 #include "Vector2.h"
 #include "Vector3.h"
 #include "Vector4.h"
+
 #include "Quaternion.h"
 #include "Matrix44.h"
+
 #include "Intersect.h"
 #include "Hermite.h"
 
