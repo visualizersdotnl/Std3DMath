96c2031c5ba0b0367c6ccb509ceb7b776de3895e
Niels J. de Wit <ndewit@gmail.com>
2026-02-23 20:38:16 +0100

    Prepared Quaternion implementation for cubic (hermite) spline interpolation (and integrating rotations)

--- Hermite.h
+++ Hermite.h
@@ -5,15 +5,17 @@
     Hermite splines are a variation on Bezier splines:
     - They use and always pass through 2 control points (each with a corresponding direction or tangent, if you will), whereas
       in bezier splines the 4 control points more or less act like attractors.
-    - The mathematical basis is more or less identical.
-
+    - Their mathematical basis is more or less identical (Bernstein polynomials).
+ 
     Two excellent resources on the matter:
-    - A proper Youtube video on Quadratric/Cubic Bezier splines by Freya Holmer: https://youtu.be/aVwxzDHniEw
-    - Article on implementing these for use with unit quaternions: https://theorangeduck.com/page/cubic-interpolation-quaternions
+    - Youtube video on Quadratric/Cubic Bezier splines by Freya Holmér: https://youtu.be/aVwxzDHniEw
+    - The Orange Duck on implementing these for use with unit quaternions: https://theorangeduck.com/page/cubic-interpolation-quaternions
 */
 
 #pragma once
 
+#include "Dependencies.h"
 namespace Std3DMath
 {
+    // To do: implement for Vector2, Vector3, Quaternion
 }
