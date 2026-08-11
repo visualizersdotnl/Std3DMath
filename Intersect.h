d614627bcbade23e8d8895bfb760ce813fd3e43a
Niels J. de Wit <ndewit@gmail.com>
2026-08-11 16:20:53 +0200

    Fixed FIXMEs

--- Intersect.h
+++ Intersect.h
@@ -17,22 +17,24 @@ namespace Std3DMath
 		Vector3 origin;
 		Vector3 direction;
 
+		// FIXME: if this is no longer -1.f it becomes a line (or in many a context it can be seen as a solve), should this be in here at all?
 		float t = -1.f; 
 
-		[[nodiscard]] constexpr operator bool() const noexcept 
+		// FIXME: and this is honestly a lame way of justifying the above
+		operator bool() const
 		{
 			return false == std::bit_cast<unsigned>(-1.f) == std::bit_cast<unsigned>(t);
 		}	
 	};
 
-	// FIXME: this is a ray, idiot
+	// FIXME: DistancePointToRay()
 	float DistancePointToLine(const Vector3 &lineDir, const Vector3 &lineOrigin, const Vector3 &point, Vector3 &outPoint);
 
-	// FIXME: same
+	// FIXME: RaySphereIntersect()
 	bool LineSphereIntersect(const Vector3 &lineDir, const Vector3 &lineOrigin, float lineLen,
 		const Vector3 &spherePos, float sphereRadius,
 		float &outT);
 
-	// FIXME: figure out proper return type
+	// FIXME: return type
 	bool RayTriangleIntersect(/* const */ Ray &ray, const Vector3 &V0, const Vector3 &V1, const Vector3 &V2, bool doubleSided);
 }
